9a8cf4a7542db3a0aa7e11f7bf0be54f1dbd7567
have2try <have2try@gmail.com>
2021-02-21 14:31:43 +0800

    add vue-router

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -13,6 +13,13 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
         publicPath: '',
     },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': path.resolve('src'),
+        }
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack starter',
