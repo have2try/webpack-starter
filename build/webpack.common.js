777910f5f6a60b6814d77fe17a2c7ee6d2923351
have2try <have2try@gmail.com>
2021-02-21 11:16:35 +0800

    Production

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -3,34 +3,29 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    mode: 'development',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        open: true
+    entry: {
+        app: './src/index.js',
     },
-    entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, '../dist'),
         publicPath: '',
     },
-    devtool: 'inline-source-map',
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Webpack starter',
         }),
         new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
     ],
     module: {
         rules: [{
             test: /\.css$/i,
             use: [MiniCssExtractPlugin.loader, 'css-loader'],
         }, {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            test: /\.(png|jpe?g|webp|git|svg|)$/i,
             type: 'asset/resource',
-        }, ],
+        }],
     },
     optimization: {
         moduleIds: 'deterministic',
@@ -44,6 +39,6 @@ module.exports = {
                 },
             },
         },
-    },
 
+    },
 };
